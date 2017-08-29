17420bd1067c068069311dd34050ea54d36191be
rhfoqkq000 <rhfoqkq000@naver.com>
2017-08-29 15:08:28 +0900

    문제 해결

--- app/src/main/java/com/donga/examples/boomin/activity/ManageLoginActivity.java
+++ app/src/main/java/com/donga/examples/boomin/activity/ManageLoginActivity.java
@@ -27,104 +27,104 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ManageLoginActivity extends AppCompatActivity {
-    AppendLog log = new AppendLog();
-    private ProgressDialog mProgressDialog;
-
-    @BindView(R.id.s_manageId)
-    EditText s_manageId;
-    @BindView(R.id.s_managePw)
-    EditText s_managePw;
-    @BindView(R.id.login_manageBt)
-    Button login_manageBt;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_manage_login);
-        ButterKnife.bind(this);
-
-        s_managePw.setOnKeyListener(new View.OnKeyListener() {
-            @Override
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
-                if(event.getAction()==KeyEvent.ACTION_DOWN){
-                    switch (keyCode)
-                    {
-                        case KeyEvent.KEYCODE_DPAD_CENTER:
-                        case KeyEvent.KEYCODE_ENTER:
-                            onLoginClicked();
-                            return true;
-                        default:
-                            break;
-                    }
-                }
-                return false;
-            }
-        });
-    }
-
-    @OnClick(R.id.login_manageBt)
-    public void onLoginClicked(){
-        Log.i("CLICK", s_manageId.getText().toString()+","+s_managePw.getText().toString());
-
-        showProgressDialog();
-        //retrofit 통신
-        Retrofit client = new Retrofit.Builder().baseUrl(getString(R.string.retrofit_url))
-                .addConverterFactory(GsonConverterFactory.create()).build();
-        Interface_authLogin authLogin = client.create(Interface_authLogin.class);
-        Call<com.donga.examples.boomin.retrofit.retrofitAuthLogin.Master> call4 =
-                authLogin.authLogin(s_manageId.getText().toString(), s_managePw.getText().toString());
-        call4.enqueue(new Callback<com.donga.examples.boomin.retrofit.retrofitAuthLogin.Master>() {
-            @Override
-            public void onResponse(Call<com.donga.examples.boomin.retrofit.retrofitAuthLogin.Master> call, Response<com.donga.examples.boomin.retrofit.retrofitAuthLogin.Master> response) {
-                if(response.body().getToken()!=null){
-                    ManageSingleton.getInstance().setToken(response.body().getToken());
-                    ManageSingleton.getInstance().setManagerID(s_manageId.getText().toString());
-                    ManageSingleton.getInstance().setManagePW(s_managePw.getText().toString());
-
-                    Intent i = new Intent(getApplicationContext(), ManageActivity.class);
-                    hideProgressDialog();
-                    startActivity(i);
-                }else {
-                    try{
-                        log.appendLog("inManageLoginActivity code not matched");
-                        Toasty.error(getApplicationContext(), "로그인 실패", Toast.LENGTH_SHORT).show();
-                    }catch(Exception e){
-                        e.printStackTrace();
-                    }
-                    hideProgressDialog();
-                }
-            }
-
-            @Override
-            public void onFailure(Call<com.donga.examples.boomin.retrofit.retrofitAuthLogin.Master> call, Throwable t) {
-                hideProgressDialog();
-                log.appendLog("inManageLoginActivity failure");
-                Toasty.error(getApplicationContext(), "로그인 실패", Toast.LENGTH_SHORT).show();
-                t.printStackTrace();
-            }
-        });
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        finish();
-    }
-
-    private void showProgressDialog() {
-        if (mProgressDialog == null) {
-            mProgressDialog = new ProgressDialog(this);
-            mProgressDialog.setMessage(getString(R.string.loading));
-            mProgressDialog.setIndeterminate(true);
-        }
-
-        mProgressDialog.show();
-    }
-
-    private void hideProgressDialog() {
-        if (mProgressDialog != null && mProgressDialog.isShowing()) {
-            mProgressDialog.hide();
-            mProgressDialog.dismiss();
-        }
-    }
+//    AppendLog log = new AppendLog();
+//    private ProgressDialog mProgressDialog;
+//
+//    @BindView(R.id.s_manageId)
+//    EditText s_manageId;
+//    @BindView(R.id.s_managePw)
+//    EditText s_managePw;
+//    @BindView(R.id.login_manageBt)
+//    Button login_manageBt;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_manage_login);
+//        ButterKnife.bind(this);
+//
+//        s_managePw.setOnKeyListener(new View.OnKeyListener() {
+//            @Override
+//            public boolean onKey(View v, int keyCode, KeyEvent event) {
+//                if(event.getAction()==KeyEvent.ACTION_DOWN){
+//                    switch (keyCode)
+//                    {
+//                        case KeyEvent.KEYCODE_DPAD_CENTER:
+//                        case KeyEvent.KEYCODE_ENTER:
+//                            onLoginClicked();
+//                            return true;
+//                        default:
+//                            break;
+//                    }
+//                }
+//                return false;
+//            }
+//        });
+//    }
+//
+//    @OnClick(R.id.login_manageBt)
+//    public void onLoginClicked(){
+//        Log.i("CLICK", s_manageId.getText().toString()+","+s_managePw.getText().toString());
+//
+//        showProgressDialog();
+//        //retrofit 통신
+//        Retrofit client = new Retrofit.Builder().baseUrl(getString(R.string.retrofit_url))
+//                .addConverterFactory(GsonConverterFactory.create()).build();
+//        Interface_authLogin authLogin = client.create(Interface_authLogin.class);
+//        Call<com.donga.examples.boomin.retrofit.retrofitAuthLogin.Master> call4 =
+//                authLogin.authLogin(s_manageId.getText().toString(), s_managePw.getText().toString());
+//        call4.enqueue(new Callback<com.donga.examples.boomin.retrofit.retrofitAuthLogin.Master>() {
+//            @Override
+//            public void onResponse(Call<com.donga.examples.boomin.retrofit.retrofitAuthLogin.Master> call, Response<com.donga.examples.boomin.retrofit.retrofitAuthLogin.Master> response) {
+//                if(response.body().getToken()!=null){
+//                    ManageSingleton.getInstance().setToken(response.body().getToken());
+//                    ManageSingleton.getInstance().setManagerID(s_manageId.getText().toString());
+//                    ManageSingleton.getInstance().setManagePW(s_managePw.getText().toString());
+//
+//                    Intent i = new Intent(getApplicationContext(), ManageActivity.class);
+//                    hideProgressDialog();
+//                    startActivity(i);
+//                }else {
+//                    try{
+//                        log.appendLog("inManageLoginActivity code not matched");
+//                        Toasty.error(getApplicationContext(), "로그인 실패", Toast.LENGTH_SHORT).show();
+//                    }catch(Exception e){
+//                        e.printStackTrace();
+//                    }
+//                    hideProgressDialog();
+//                }
+//            }
+//
+//            @Override
+//            public void onFailure(Call<com.donga.examples.boomin.retrofit.retrofitAuthLogin.Master> call, Throwable t) {
+//                hideProgressDialog();
+//                log.appendLog("inManageLoginActivity failure");
+//                Toasty.error(getApplicationContext(), "로그인 실패", Toast.LENGTH_SHORT).show();
+//                t.printStackTrace();
+//            }
+//        });
+//    }
+//
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//        finish();
+//    }
+//
+//    private void showProgressDialog() {
+//        if (mProgressDialog == null) {
+//            mProgressDialog = new ProgressDialog(this);
+//            mProgressDialog.setMessage(getString(R.string.loading));
+//            mProgressDialog.setIndeterminate(true);
+//        }
+//
+//        mProgressDialog.show();
+//    }
+//
+//    private void hideProgressDialog() {
+//        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+//            mProgressDialog.hide();
+//            mProgressDialog.dismiss();
+//        }
+//    }
 }
---
+++ app/src/main/java/com/donga/examples/boomin/fragment/Res_BuminFragment.java
@@ -0,0 +1,162 @@
+package com.donga.examples.boomin.fragment;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.donga.examples.boomin.R;
+import com.donga.examples.boomin.retrofit.retrofitMeal.Interface_meal;
+import com.donga.examples.boomin.retrofit.retrofitMeal.Master3;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import es.dmoral.toasty.Toasty;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by horse on 2017. 8. 29..
+ */
+
+public class Res_BuminFragment extends Fragment{
+    private ProgressDialog mProgressDialog;
+    public int count = 0;
+
+    @BindView(R.id.date_text)
+    TextView date_text;
+    @BindView(R.id.pre_res)
+    ImageView pre_res;
+    @BindView(R.id.next_res)
+    ImageView next_res;
+
+    @BindView(R.id.guk)
+    TextView guk;
+    @BindView(R.id.gang)
+    TextView gang;
+    @BindView(R.id.bumin)
+    TextView bumin;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        final View rootview = inflater.inflate(R.layout.fragment_res_bumin, container, false);
+        ButterKnife.bind(this, rootview);
+        SimpleDateFormat msimpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date currentTime = new Date();
+        String now = msimpleDateFormat.format(currentTime);
+
+
+        retrofit(now);
+
+        date_text.setText(now);
+//        date_text.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                DialogFragment newFragment = new CalendarFragment();
+//                newFragment.show(getSupportFragmentManager(), "Date Picker");
+//            }
+//        });
+
+        pre_res.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                count = count - 1;
+
+                Calendar cal = Calendar.getInstance();
+                cal.add(Calendar.DATE, count); // +1은 내일
+
+                SimpleDateFormat date = new SimpleDateFormat("yyyy-MM-dd");
+                String pre = date.format(cal.getTime());
+                retrofit(pre);
+                date_text.setText(pre);
+            }
+        });
+        next_res.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                count = count + 1;
+
+                Calendar cal = Calendar.getInstance();
+                cal.add(Calendar.DATE, count); // +1은 내일
+
+                SimpleDateFormat date = new SimpleDateFormat("yyyy-MM-dd");
+                String next = date.format(cal.getTime());
+
+                retrofit(next);
+                date_text.setText(next);
+            }
+        });
+        return rootview;
+    }
+    public void retrofit(String getTime) {
+
+        showProgressDialog();
+
+        Retrofit client = new Retrofit.Builder().baseUrl(getString(R.string.retrofit_url))
+                .addConverterFactory(GsonConverterFactory.create()).build();
+        Interface_meal meal = client.create(Interface_meal.class);
+        retrofit2.Call<Master3> call3 = meal.getMeal(getTime);
+        call3.enqueue(new Callback<Master3>() {
+            @Override
+            public void onResponse(Call<Master3> call, Response<Master3> response) {
+                if (response.body().getResult_code() == 1) {
+                    String source_guk = response.body().getResult_body().getInter();
+                    guk.setText(Html.fromHtml(source_guk));
+                    guk.setMovementMethod(LinkMovementMethod.getInstance());
+
+                    String source_bumin = response.body().getResult_body().getBumin_kyo();
+                    bumin.setText(Html.fromHtml(source_bumin));
+                    bumin.setMovementMethod(LinkMovementMethod.getInstance());
+
+                    String source_gang = response.body().getResult_body().getGang();
+                    gang.setText(Html.fromHtml(source_gang));
+                    gang.setMovementMethod(LinkMovementMethod.getInstance());
+
+                    hideProgressDialog();
+                } else {
+//                    log.appendLog("inResActivity code not matched");
+                    Toasty.error(getContext(), "불러오기 실패", Toast.LENGTH_SHORT).show();
+                    hideProgressDialog();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Master3> call, Throwable t) {
+//                log.appendLog("inResActivity failure");
+                hideProgressDialog();
+                Toasty.error(getContext(), "불러오기 실패", Toast.LENGTH_SHORT).show();
+                t.printStackTrace();
+            }
+        });
+    }
+
+    private void showProgressDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(getContext());
+            mProgressDialog.setMessage(getString(R.string.loading));
+            mProgressDialog.setIndeterminate(true);
+        }
+
+        mProgressDialog.show();
+    }
+
+    private void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.hide();
+            mProgressDialog.dismiss();
+        }
+    }
+}
